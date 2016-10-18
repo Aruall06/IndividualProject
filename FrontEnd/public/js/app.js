d0d5ac7d7e5f1a229b47bf1e98a3dddb9b045a79
Laura Salivonaite <laura.salivonaite@skybettingandgaming.com>
2016-10-18 16:09:29 +0100

    database and index page

--- FrontEnd/public/js/app.js
+++ FrontEnd/public/js/app.js
@@ -1,13 +1,79 @@
 $(function() {
 
-    $('#enter').click(function() {
-        window.location.href = '../pages/gallery.html';
-        console.log("clicked");
+    $registerform = $('#register-section')
+    $loginform = $('#login-section')
+
+    $loginform.hide();
+    $registerform.hide();
+
+    $('#login').click(function(e) {
+    $loginform.show();
+    $registerform.hide();
     });
 
-    lightbox.option({
-        'resizeDuration': 500,
-        'wrapAround': true
-    })
+    $('#register').click(function(e) {
+      $loginform.hide();
+      $registerform.show();
+    });
 
-})
+    $('#login-button').click(function(e) {
+    var username = $('#username').val();
+    var password = $('#password').val();
+    loginRequest(username, password);
+  });
+
+
+  $('#register-button').click(function(e) {
+    var username = $('#registerusername').val();
+    var password = $('#registerpassword').val();
+    registerRequest(username, password);
+  });
+
+});
+
+function registerRequest(username, password) {
+  $.ajax({
+    url: "http://localhost:3000/api/users",
+    type: 'POST',
+    dataType: 'json',
+    data: {
+      "username": username,
+      "password": password
+    },
+    async: false,
+    statusCode: {
+      201: function(response) {
+        $('#message').append('<p>You have successfully registered. Now you can login</p>');
+      },
+    }
+  });
+}
+
+function loginRequest(username, password) {
+  $.ajax({
+    url: "http://localhost:3000/api/users/login",
+    type: 'POST',
+    dataType: 'json',
+    data: {
+      "username": username,
+      "password": password
+    },
+    async: false,
+    statusCode: {
+      200: function(response) {
+        localStorage.setItem('user', response.user);
+        localStorage.setItem('username', response.username);
+
+        $('#userMessage').append('<h1>Welcome, ' + localStorage.getItem('username') + '</h1>')
+        console.log("logged in");
+            window.location.href = '../pages/gallery.html';
+      },
+      400: function(response) {
+        $('message').html("<p>A user does not exist with the given details</p>");
+        $('#username').val("");
+        $('#password').val("");
+        console.log("doesnt work");
+      }
+    }
+  });
+}
